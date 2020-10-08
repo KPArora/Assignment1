62e001a17095311b6149efb759474bc321f3d29b
KPArora <kanu94angel@gmail.com>
2020-10-09 05:02:59 +0530

    My first project

---
+++ Assignment1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Assignment1
+{
+    class Program
+    {
+        public static bool Menu()
+        {
+            Console.WriteLine("Select 1 to Enter Triangle dimensions ");
+            Console.WriteLine("Select 2 to Exit");
+            string input;
+            input = Console.ReadLine();
+            //ask user to select choice
+            if (input == "1")
+            {
+                int side1, side2, side3;
+                Console.Write("Input side 1 of triangle: ");
+                side1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Input side 2 of triangle: ");
+                side2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Input side 3 of triangle: ");
+                side3 = Convert.ToInt32(Console.ReadLine());
+
+                string test = TriangleSolver.Analyze(side1, side2, side3);
+                Console.WriteLine(test);
+            }
+            else if (input == "2")
+            {
+                return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            bool x = true;
+            while (x == true)
+            {
+                x = Menu();
+            }
+        }
+    }
+}
---
+++ Assignment1/TriangleSolver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment1
+{
+    public static class TriangleSolver
+    {
+        public static string Analyze(int sidea, int sideb, int sidec) 
+        {
+
+            if (sidea + sideb <= sidec || sidea + sidec <= sideb || sideb + sidec <= sidea)
+            {
+                return "The triangle is invalid";
+            }
+            else
+            {
+                if (sidea == sideb && sideb == sidec)
+                {
+                    return "This is an equilateral triangle.";
+                }
+                else if (sidea == sideb || sidea == sidec || sideb == sidec)
+                {
+                    return "This is an isosceles triangle.";
+                }
+                else
+                {
+                    return "This is a scalene triangle";
+                }
+
+            }
+        }
+
+    }
+}
